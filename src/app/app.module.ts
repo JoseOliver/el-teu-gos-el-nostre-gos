41aa8f82f203ecc8ea622246f34de57922230c03
Jose Andrés <oliver_ja@outlook.com>
2023-04-15 19:15:54 +0200

    feat: navbar menu button now shows a side div menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavbarComponent } from './components/navbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MainComponent } from './views/main/main.component';
 
 // material
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    AppComponent
+    AppComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule, MatIconModule, MatButtonModule // material
+    MatToolbarModule, MatIconModule, MatButtonModule, MatSidenavModule // material
   ],
   providers: [],
   bootstrap: [AppComponent]
